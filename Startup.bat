f31550a4df887957fcc311446d119acfbb7ee00d
JhoimarSilva <99420542+JhoimarSilva@users.noreply.github.com>
2023-02-03 20:26:11 -0500

    Delete Startup.bat

--- Startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-CALL cd "%~dp0"Frontend
-CALL npm run build
-CALL cd ..
-CALL cd "%~dp0"backend
-::CAMBIAR RUTA A DONDE TIENEN PYTHON INSTALADO, AQUI ->>>>>>>>>>>>>>>>>>
-CALL "%LocalAppData%"\Programs\Python\Python310\python.exe manage.py runserver
-pause
